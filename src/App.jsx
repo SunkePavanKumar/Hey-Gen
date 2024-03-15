3b7acc4bd43ebf9c9ddc1019bd08e26139d3837f
Pavan Kumar Sunke <pavan.1413489@gmail.com>
2024-03-15 16:05:45 +0530

    COM : Deleted the markting page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Footer from "./components/Footer";
 import ImageCarousel from "./components/ImageCarousel";
 import Intro from "./components/Intro";
-import Marketing from "./components/Marketing";
+// import Marketing from "./components/Marketing";
 import NavBar from "./components/NavBar";
 import TypeAnimationBox from "./components/TypeAnimationBox";
 function App() {
@@ -12,7 +12,7 @@ function App() {
       <Intro />
       <ImageCarousel />
       <TypeAnimationBox />
-      <Marketing />
+      {/* <Marketing /> */}
       <Footer />
     </div>
   );
--- src/components/Marketing.jsx
+++ src/components/Marketing.jsx
@@ -1,58 +1,58 @@
-function Marketing() {
-  return (
-    <div className="container bg-[#6b8dd2] mt-8  h-auto ml-auto mr-auto w-full">
-      <div className="w-[92%] h-auto ml-auto mr-auto">
-        <div className="flex">
-          <h2 className="max-w-742 text-neutral-10 tracking-wider text-[123px] font-medium leading-none text-white">
-            Create videos for every team
-          </h2>
-          <p className="text-neutral-10 text-base text-[24px] text-white relative top-[177px] right-[134px]">
-            Engage, explain, entertain - AI-Powered video creation for any need.
-          </p>
-        </div>
-        <div className=" flex items-center justify-center gap-4 flex-wrap mt-12">
-          <div className=" w-[570px] h-[170px]">
-            <video
-              src="https://resource.heygencdn.com/homepage/usecase_sales2.mp4"
-              type="video/mp4"
-              muted
-              autoplay
-              loop
-            ></video>
-          </div>
-          <div className=" w-[570px] h-[170px]">
-            {" "}
-            <video
-              src="https://resource.heygencdn.com/homepage/usecase_productmarketing2.mp4"
-              type="video/mp4"
-              muted
-              autoplay
-              loop
-            ></video>
-          </div>
-          <div className=" w-[570px] h-[170px]">
-            <video
-              src="https://resource.heygencdn.com/homepage/usecase_mailchimp2.mp4"
-              type="video/mp4"
-              muted
-              autoplay
-              loop
-            ></video>
-          </div>
-          <div className=" w-[570px] h-[170px]">
-            <video
-              class="muted-video"
-              src="https://resource.heygencdn.com/website/vera_remove/usecase_lnd2_new_output.mp4"
-              type="video/mp4"
-              muted=""
-              autoplay=""
-              loop=""
-            ></video>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
+// function Marketing() {
+//   return (
+//     <div className="container bg-[#6b8dd2] mt-8  h-auto ml-auto mr-auto w-full">
+//       <div className="w-[92%] h-auto ml-auto mr-auto">
+//         <div className="flex">
+//           <h2 className="max-w-742 text-neutral-10 tracking-wider text-[123px] font-medium leading-none text-white">
+//             Create videos for every team
+//           </h2>
+//           <p className="text-neutral-10 text-base text-[24px] text-white relative top-[177px] right-[134px]">
+//             Engage, explain, entertain - AI-Powered video creation for any need.
+//           </p>
+//         </div>
+//         <div className=" flex items-center justify-center gap-4 flex-wrap mt-12">
+//           <div className=" w-[570px] h-[170px]">
+//             <video
+//               src="https://resource.heygencdn.com/homepage/usecase_sales2.mp4"
+//               type="video/mp4"
+//               muted
+//               autoplay
+//               loop
+//             ></video>
+//           </div>
+//           <div className=" w-[570px] h-[170px]">
+//             {" "}
+//             <video
+//               src="https://resource.heygencdn.com/homepage/usecase_productmarketing2.mp4"
+//               type="video/mp4"
+//               muted
+//               autoplay
+//               loop
+//             ></video>
+//           </div>
+//           <div className=" w-[570px] h-[170px]">
+//             <video
+//               src="https://resource.heygencdn.com/homepage/usecase_mailchimp2.mp4"
+//               type="video/mp4"
+//               muted
+//               autoplay
+//               loop
+//             ></video>
+//           </div>
+//           <div className=" w-[570px] h-[170px]">
+//             <video
+//               class="muted-video"
+//               src="https://resource.heygencdn.com/website/vera_remove/usecase_lnd2_new_output.mp4"
+//               type="video/mp4"
+//               muted=""
+//               autoplay=""
+//               loop=""
+//             ></video>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default Marketing;
+// export default Marketing;
